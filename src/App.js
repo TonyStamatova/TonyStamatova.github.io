a726885524e1a1e007b6ed452907ccf6fd60494f
TonyStamatova <a.stamatova@abv.bg>
2021-06-12 13:49:54 +0300

    Add content div and routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import './App.css';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import About from "./components/About.jsx";
+// import Projects from "./components/Projects.jsx";
+// import Interests from "./components/Interests.jsx";
+import ContactMe from "./components/ContactMe.jsx";
 
 function App() {
   return (
+    <HashRouter>
     <div className="App">
       <header className="App-header">
         <nav id="nav">
             <ul>
-                <li><a class="active" href="index.html">About</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Interests</a></li>
-                <li><a href="">Contact Me</a></li>
+                <li><NavLink to="/">About</NavLink></li>
+                {/* <li><NavLink to="/projects">Projects</NavLink></li>
+                <li><NavLink to="/interests">Interests</NavLink></li> */}
+                <li><NavLink to="/contactMe">Contact Me</NavLink></li>
             </ul>
         </nav>
         <h1 className="logo">Antoniya Stamatova</h1>
         <hr />
         <h2 className="slogan">Software Developer</h2>
       </header>
-      <main></main>
+      <div className="content">
+        <Route exact path="/" component={About}/>
+        {/* <Route path="/projects" component={Projects}/>
+        <Route path="/interests" component={Interests}/> */}
+        <Route path="/contactMe" component={ContactMe}/>
+      </div>
       <footer id="footer">
             <div class="footer-content">
                 <a href="#" class="fa fa-phone"></a>
@@ -27,6 +42,7 @@ function App() {
             </div>
       </footer>
     </div>
+    </HashRouter>
   );
 }
 
